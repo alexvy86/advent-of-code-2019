6431530b509056347e71009cf99357283b5a04a2
Alex Villarreal <alex@alexvy86.com>
2020-09-13 19:04:05 +0000

    Day 3 - part 2

--- day3.py
+++ day3.py
@@ -4,58 +4,86 @@ wire_paths = utils.read_lines(3)
 
 visited = set()
 intersections = set()
+steps_wire_1 = {}
+steps_wire_2 = {}
 
 path_1 = wire_paths[0]
-current_x = current_y = 0
+current_x = current_y = steps = 0
 for instruction in path_1.split(","):
 	direction = instruction[0]
 	distance = int(instruction[1:])
 	if direction == "U":
 		for _ in range(distance):
 			current_y += 1
+			steps += 1
 			visited.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_1:
+				steps_wire_1[(current_x, current_y)] = steps
 	elif direction == "R":
 		for _ in range(distance):
 			current_x += 1
+			steps += 1
 			visited.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_1:
+				steps_wire_1[(current_x, current_y)] = steps
 	elif direction == "D":
 		for _ in range(distance):
 			current_y -= 1
+			steps += 1
 			visited.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_1:
+				steps_wire_1[(current_x, current_y)] = steps
 	elif direction == "L":
 		for _ in range(distance):
 			current_x -= 1
+			steps += 1
 			visited.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_1:
+				steps_wire_1[(current_x, current_y)] = steps
 
 path_2 = wire_paths[1]
-current_x = current_y = 0
+current_x = current_y = steps = 0
 for instruction in path_2.split(","):
 	direction = instruction[0]
 	distance = int(instruction[1:])
 	if direction == "U":
 		for _ in range(distance):
 			current_y += 1
+			steps += 1
 			if (current_x, current_y) in visited:
 				intersections.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_2:
+				steps_wire_2[(current_x, current_y)] = steps
 	elif direction == "R":
 		for _ in range(distance):
 			current_x += 1
+			steps += 1
 			if (current_x, current_y) in visited:
 				intersections.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_2:
+				steps_wire_2[(current_x, current_y)] = steps
 	elif direction == "D":
 		for _ in range(distance):
 			current_y -= 1
+			steps += 1
 			if (current_x, current_y) in visited:
 				intersections.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_2:
+				steps_wire_2[(current_x, current_y)] = steps
 	elif direction == "L":
 		for _ in range(distance):
 			current_x -= 1
+			steps += 1
 			if (current_x, current_y) in visited:
 				intersections.add((current_x, current_y))
+			if (current_x, current_y) not in steps_wire_2:
+				steps_wire_2[(current_x, current_y)] = steps
 
-answer = 1e10
+best_distance = best_sum_of_steps = 1e10
 for candidate in intersections:
 	distance = abs(candidate[0]) + abs(candidate[1])
-	answer = min(answer, distance)
+	best_distance = min(best_distance, distance)
+	best_sum_of_steps = min(best_sum_of_steps, steps_wire_1[candidate] + steps_wire_2[candidate])
 
-print(answer)
+print(best_distance)
+print(best_sum_of_steps)
