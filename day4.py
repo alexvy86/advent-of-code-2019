36bde9cecac26208607d3e1a2dd723421d2c6f06
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-05 20:50:38 -0500

    Day 4 - part 2

--- day4.py
+++ day4.py
@@ -9,21 +9,32 @@ how_many_passwords = 0
 for candidate in range(effective_min, effective_max + 1):
 	candidate_string = str(candidate)
 
-	meets_criteria = True
+	non_decreasing_digits = True
 
-	adjacent_equal_digits = False
-	
 	for c in range(len(candidate_string)-1):
 		if int(candidate_string[c+1]) < int(candidate_string[c]):
-			meets_criteria = False		
-		
-		adjacent_equal_digits |= candidate_string[c+1] == candidate_string[c]
-	
-	if not adjacent_equal_digits:
-		meets_criteria = False
+			non_decreasing_digits = False		
 
-	if meets_criteria:
-	  how_many_passwords += 1
+	if not non_decreasing_digits:
+		continue
+
+	idx = 0
+	has_sequence_of_exactly_2_equal_digits = False
+
+	while idx < len(candidate_string):
+		current_digit = candidate_string[idx]
+		seq_length = 0
+		while idx < len(candidate_string) and candidate_string[idx] == current_digit:
+			idx += 1
+			seq_length += 1
+		if seq_length == 2:
+			has_sequence_of_exactly_2_equal_digits = True
+			break
+		
+	if not has_sequence_of_exactly_2_equal_digits:
+		continue
+	
+	how_many_passwords += 1
 
 print(how_many_passwords)
 
