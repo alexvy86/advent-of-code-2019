adb3d09253b040c9ad013e40d9255758202ea058
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-05 20:50:39 -0500

    Code cleanup

--- intcode_computer.py
+++ intcode_computer.py
@@ -25,20 +25,14 @@ class IntcodeComputer:
 
 			#print(f"op_code: {op_code}")
 			if op_code == 1:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 3]
+				params = self._get_parameter_values(2, instruction_pointer)
 				output_address = self.program[instruction_pointer + 3]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
 				self.program[output_address] = params[0] + params[1]
 				instruction_pointer += 4
 			
 			elif op_code == 2:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 3]
+				params = self._get_parameter_values(2, instruction_pointer)
 				output_address = self.program[instruction_pointer + 3]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
 				self.program[output_address] = params[0] * params[1]
 				instruction_pointer += 4
 			
@@ -49,51 +43,27 @@ class IntcodeComputer:
 				instruction_pointer += 2
 			
 			elif op_code == 4:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 2]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
-				
+				params = self._get_parameter_values(1, instruction_pointer)
 				print(params[0])
 				instruction_pointer += 2
 
 			elif op_code == 5:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 3]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
+				params = self._get_parameter_values(2, instruction_pointer)
+				instruction_pointer = params[1] if params[0] != 0 else instruction_pointer + 3
 				
-				if params[0] != 0:
-					instruction_pointer = params[1]
-				else:
-					instruction_pointer += 3
-
 			elif op_code == 6:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 3]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
+				params = self._get_parameter_values(2, instruction_pointer)
+				instruction_pointer = params[1] if params[0] == 0 else instruction_pointer + 3
 				
-				if params[0] == 0:
-					instruction_pointer = params[1]
-				else:
-					instruction_pointer += 3
-
 			elif op_code == 7:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 3]
+				params = self._get_parameter_values(2, instruction_pointer)
 				output_address = self.program[instruction_pointer + 3]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
 				self.program[output_address] = 1 if params[0] < params[1] else 0
 				instruction_pointer += 4
 
 			elif op_code == 8:
-				params = self.program[instruction_pointer + 1 : instruction_pointer + 3]
+				params = self._get_parameter_values(2, instruction_pointer)
 				output_address = self.program[instruction_pointer + 3]
-				for i in range(len(params)):	
-					if i >= len(param_modes) or param_modes[i] == "0":
-						params[i] = self.program[params[i]]
 				self.program[output_address] = 1 if params[0] == params[1] else 0
 				instruction_pointer += 4
 
@@ -102,3 +72,12 @@ class IntcodeComputer:
 
 	def set_address(self, address: int, value: int):
 		self.program[address] = value
+
+	def _get_parameter_values(self, num_params, ip):
+		params = self.program[ip + 1 : ip + 1 + num_params]
+		param_modes = list(str(self.program[ip] // 100))
+		param_modes.reverse()
+		for i in range(len(params)):	
+			if i >= len(param_modes) or param_modes[i] == "0":
+				params[i] = self.program[params[i]]
+		return params
